816e56f81332c09e07833e33ef1bb5c089f4bd85
Fabián Núñez <fanuneza@gmail.com>
2021-12-21 13:02:57 -0300

    Trabajo terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// Componente JS 2: ScrollSpy
+
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
   target: "#navbar-example",
+});
+
+// Componente JS 3: Tooltips
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
 });
